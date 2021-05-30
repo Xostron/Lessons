2a9f1bb8420198ae988e0542e471da922fb61711
Xostron <aat9044107480@gmail.com>
2021-05-30 23:32:57 +0300

    TODO conversion function

--- For FunTest/ASCII Art.py
+++ For FunTest/ASCII Art.py
@@ -10,16 +10,30 @@ E
 
 
 https://www.codingame.com/ide/puzzle/ascii-art
+
+
+
 """
+# TODO function to convert input data (A..Z?) to dictionary {"A": ([x,x,x,x],[x,x,x,x],[x,x,x,x],[x,x,x,x],[x,x,x,x])}
+# [x,x,x,x] - element in row = width_symbol
+# [x,x,x,x],[x,x,x,x],[x,x,x,x],[x,x,x,x],[x,x,x,x] - number of rows =  height_symbol
+
+def rawToDict(width_symbol, height_symbol, inputData):
+    pass
 
 width_symbol = int(input())
 height_symbol = int(input())
 symbol = input()
 graphicsList = []
 
+
+
+
 while height_symbol>0:
     graphicsList.append([i for i in input()])
     height_symbol -= 1
 
+
+
 print(len(graphicsList))
-print(graphicsList[1])
+print(graphicsList)
