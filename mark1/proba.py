63e65debe92e6127621c9e431b91a1f2132b318d
Xostron <aat9044107480@gmail.com>
2021-06-18 12:01:09 +0400

    test get post @fasttranslate

--- mark1/proba.py
+++ mark1/proba.py
@@ -1,9 +1,29 @@
 import requests, json
 
 
-data = {'source': 'ленточный конвейер'}
-s = requests.get(r'https://fasttranslator.herokuapp.com/api/v1/detect', params=data)
+# data = {'source': 'ленточный конвейер'}
+# s = requests.get(r'https://fasttranslator.herokuapp.com/api/v1/detect', params=data)
+# data = s.json()
+# print(data['Lang'])
+#
+#
+#
+# url_post = 'https://fasttranslator.herokuapp.com/api/v1/text/to/text'
+# data_post = {
+#     'source': 'allure',
+#     'lang': 'en-ru'
+# }
+# r = requests.post(url=url_post, data=data_post)
+#
+# out = r.json()
+# print(out)
 
-data = s.json()
 
-print(data['Lang'])
+# u = 'https://translate.yandex.ru/?utm_source=wizard&lang=ru-en&text=привет'
+# https://translate.yandex.ru/?utm_source=wizard&lang=ru-en
+# https://translate.yandex.ru/?utm_source=wizard&lang=ru-en
+# &text=привет%20меня%20зовут
+u1 = 'https://translate.yandex.ru/?utm_source=wizard&lang=ru-en&text=привет%20меня%20зовут'
+s = requests.get(u1)
+print(s.text.encode('utf-8'))
+
