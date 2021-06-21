23e5eaa14762c8dd14619e9130e955f210d808b0
Xostron <aat9044107480@gmail.com>
2021-06-21 16:59:58 +0400

    test get post https://developers.lingvolive.com

--- mark1/proba.py
+++ mark1/proba.py
@@ -1,29 +1,55 @@
 import requests, json
 
-
-# data = {'source': 'ленточный конвейер'}
-# s = requests.get(r'https://fasttranslator.herokuapp.com/api/v1/detect', params=data)
-# data = s.json()
-# print(data['Lang'])
+API_KEY = 'N2Q1MWFjM2ItZWQzMi00Mjk0LWEyZDQtYTY5YmNmZjgzYzVkOjA2MTEwNmJlN2U2YTRmOWFhYjg0MjBlNDlhM2UyZjI2'
+URL_AUTH = 'https://developers.lingvolive.com/api/v1.1/authenticate'
+# headers_auth = {'Authorization': 'Basic ' + API_KEY}
 #
+# token_auth = requests.post(url=URL_AUTHENTICATE, headers=headers_auth)
+# token = token_auth.text
 #
+# print(token_auth.status_code)
+# print(token)
 #
-# url_post = 'https://fasttranslator.herokuapp.com/api/v1/text/to/text'
-# data_post = {
-#     'source': 'allure',
-#     'lang': 'en-ru'
+url = 'https://developers.lingvolive.com/api/v1/Minicard'
+# params = {
+#     'text': 'always',
+#     'srcLang': 1033,
+#     'dstLang': 1049
 # }
-# r = requests.post(url=url_post, data=data_post)
-#
-# out = r.json()
-# print(out)
+# head = {'Authorization': 'Bearer ' + token}
+# s = requests.get(url=url, headers=head, params=params)
+# res = s.json()
+# print(res['Translation']['Translation'])
 
 
-# u = 'https://translate.yandex.ru/?utm_source=wizard&lang=ru-en&text=привет'
-# https://translate.yandex.ru/?utm_source=wizard&lang=ru-en
-# https://translate.yandex.ru/?utm_source=wizard&lang=ru-en
-# &text=привет%20меня%20зовут
-u1 = 'https://translate.yandex.ru/?utm_source=wizard&lang=ru-en&text=привет%20меня%20зовут'
-s = requests.get(u1)
-print(s.text.encode('utf-8'))
+class Translate:
 
+    def __init__(self, url, key):
+        pass
+        self.API_KEY = key
+        self.URL_AUTH = url
+        self.headers = {
+            'Authorization':'Basic '+ self.API_KEY
+        }
+        self.auth()
+
+    def auth(self):
+        token_auth = requests.post(url=self.URL_AUTH, headers=self.headers)
+        self.token = token_auth.text
+        self.token_status = token_auth.status_code
+
+    def translate(self, url, word):
+        header = {'Authorization': 'Bearer ' + self.token}
+        params = {
+            'text': word,
+            'srcLang': 1033,
+            'dstLang': 1049
+        }
+        self.order = requests.get(url=url, headers=header, params=params)
+        self.result = self.order.json()
+        self.trans_word = self.result['Translation']['Translation']
+
+
+t = Translate(URL_AUTH, API_KEY)
+t.translate(url, 'Happy')
+print(t.trans_word)
