a1cc8f4ef8e9147a2381d335097980636a32744c
Xostron <aat9044107480@gmail.com>
2021-06-25 14:56:54 +0400

    test

--- mark1/kivy_layouts.py
+++ mark1/kivy_layouts.py
@@ -13,18 +13,18 @@ class myPrj(App):
 
 
         a1 = AnchorLayout(anchor_x='center', anchor_y='center')
-        # a1.add_widget(Button(text='A1', size_hint=[.5,.5]))
+        a1.add_widget(Button(text='A1', size_hint=[1,1]))
 
-        # g1 = GridLayout(rows=5, padding=25, spacing=5)
-        # for i in range(10):
-        #     g1.add_widget(Button(text=f'G{i+1}'))
+        g1 = GridLayout(rows=10, padding=25, spacing=5)
+        for i in range(10):
+            g1.add_widget(Button(text=f'G{i+1}'))
+        a1.add_widget(g1)
 
-
-        b1 = BoxLayout(orientation='vertical', size_hint=[.5, .5], spacing=10)
-        b1.add_widget(TextInput(text='Login'))
-        b1.add_widget(TextInput(text='Password'))
-        b1.add_widget(Button(text='Login'))
-        a1.add_widget(b1)
+        # b1 = BoxLayout(orientation='vertical', size_hint=[.5, .5], spacing=10)
+        # b1.add_widget(TextInput(text='Login'))
+        # b1.add_widget(TextInput(text='Password'))
+        # b1.add_widget(Button(text='Login'))
+        # a1.add_widget(b1)
         return a1
 
 
