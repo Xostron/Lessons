e1fef0f2491fac6d9a5b440bdaedcf3f10e4bf20
Xostron <aat9044107480@gmail.com>
2021-07-06 11:48:06 +0400

    80% сделано

--- mark2/Story.py
+++ mark2/Story.py
@@ -17,6 +17,7 @@ from kivy.core.audio import SoundLoader
 from kivy.uix.carousel import Carousel
 from kivy.uix.image import AsyncImage, Image
 from kivy.uix.label import Label
+from kivy.properties import AliasProperty
 
 page = ['У каждого человека свои звезды.\n '
         'Хотел бы я знать, зачем звезды светятся.\n',
@@ -35,39 +36,43 @@ page = ['У каждого человека свои звезды.\n '
 
 class CarouselApp(App):
     def myChange(self, instance, value):
-        print(self.carousel.next_slide, self.next_slide,'\n', self.carousel.current_slide,)
-        if self.next_slide == self.carousel.current_slide:
-            self.next_slide = self.carousel.next_slide
-            if self.count < 9:
-                self.count += 1
-            else:
-                self.count = 9
+        #print(self.carousel.scroll_distance)
+        #print(self.carousel.canvas)
+        self.get_right()
+        print(self.right)
+        pass
 
-        self.text.text = page[self.count]
 
     def build(self):
         global page
         self.count = 0
+
         self.carousel = Carousel(direction='right')
         self.carousel.bind(on_touch_move=self.myChange)
+        self.carousel.scroll_distance = 10
+        self.carousel.scroll_timeout = 999
+        #self.carousel.
+
+
         box = BoxLayout(orientation='vertical', padding=3)
         self.text = Label(text=page[self.count],
-                     pos=(10, 0),
-                     width=300,
-                     height=100)
+                          pos=(10, 0),
+                          width=300,
+                          height=100)
         for i in range(10):
             source = f'{i}.jpg'
-            image = Image(source=source)
-            self.carousel.add_widget(image)
-        self.next_slide = self.carousel.next_slide
+            self.image = Image(source=source)
+            self.carousel.add_widget(self.image)
+        self.slide = self.carousel.next_slide
         box.add_widget(self.carousel)
         box.add_widget(self.text)
         return box
 
 
-# sound = SoundLoader.load('saib. - Shanghai Nights.mp3')
-# if sound:
-#     print("Sound found at %s" % sound.source)
-#     print("Sound is %.3f seconds" % sound.length)
-#     sound.play()
+sound = SoundLoader.load('saib. - Shanghai Nights.mp3')
+if sound:
+    print("Sound found at %s" % sound.source)
+    print("Sound is %.3f seconds" % sound.length)
+    sound.play()
+
 CarouselApp().run()
