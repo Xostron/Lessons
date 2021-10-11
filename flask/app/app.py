57437dbc0ade114f21acba166fb1e5cba084a1ce
Xostron <aat9044107480@gmail.com>
2021-10-11 16:15:34 +0300

    learning flask (https://www.youtube.com/watch?v=Y_oyx36AdV0&list=PLlWXhlUMyooZr5R2u2Zwxt6Pw6iwBo5y5)

--- flask/app/app.py
+++ flask/app/app.py
@@ -1,5 +1,10 @@
 from flask import Flask
 from config import Configuration
 
+from flask_sqlalchemy import SQLAlchemy     # ORM (object relation model) system
+
+
 app = Flask(__name__)
 app.config.from_object(Configuration)
+db = SQLAlchemy(app)
+
